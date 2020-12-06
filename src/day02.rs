d823982d3b9efc89f0901d4e876511f342305345
Adrian E <addis.aden@gmail.com>
2020-12-06 12:35:00 +0100

    day02 part b doesn't work

--- src/day02.rs
+++ src/day02.rs
@@ -16,6 +16,35 @@ fn is_valid (lower: usize, upper: usize, single_char: String, validation_string:
     found >= lower && found <= upper
 }
 
+fn is_valid_2 (lower: usize, upper: usize, single_char: String, validation_string: String) -> bool {
+    let mut lower_match : bool = false;
+    let mut upper_match : bool = false;
+    for x in 0..validation_string.len() {
+        match validation_string[x..].find(single_char.as_str()) {
+            None => {
+                if x == lower - 1 {
+                    lower_match = false;
+                }
+                if x == upper - 1 {
+                    upper_match = false;
+                }
+            },
+            Some(y) => {
+                if x == lower - 1 && y == 0 {
+                    lower_match = true;
+                }
+                if x == upper - 1 && y == 0 {
+                    upper_match = true;
+                }
+            },
+        }
+    }
+    if !(lower_match && !upper_match) {
+        println!("{} {} {} {}", lower, upper, single_char, validation_string);
+    }
+    lower_match && !upper_match
+}
+
 fn parse_line (line: String) -> Option<(usize, usize, String, String)> {
     let result: Vec<&str> = line.rsplitn(2, ":").collect();
     if result.len() != 2 {
@@ -66,12 +95,44 @@ pub fn day02a() -> usize {
     valid_passwords
 }
 
+pub fn day02b() -> usize {
+    let filename = "inputs/02/input.txt";
+
+    let content = fs::read_to_string(filename).expect("");
+
+    let splittet : Vec<&str> = content.split("\n").collect();
+
+    let mut valid_passwords : usize = 0;
+
+    for a in splittet {
+        match parse_line(String::from(a)) {
+            None => print!(""),
+            Some((lower, upper, chr, stri)) => {
+                if is_valid_2(lower, upper, String::from(chr), String::from(stri)) {
+                    valid_passwords += 1;
+                }
+            }
+        }
+    }
+
+    valid_passwords
+}
+
+
 #[test]
 fn test_validation() {
     assert_eq!(is_valid(1, 3, String::from("a"), String::from("abcde")), true, "abcde valid");
     assert_eq!(is_valid(1, 3, String::from("b"), String::from("cdefg")), false, "cdefg invalid");
     assert_eq!(is_valid(2, 9, String::from("c"), String::from("ccccccccc")), true, "ccccccccc valid");
     assert_eq!(is_valid(2, 9, String::from("c"), String::from("cccccccccc")), false, "cccccccccc valid");
+}
+
+#[test]
+fn test_validation_part_2() {
+    assert_eq!(is_valid_2(1, 3, String::from("a"), String::from("abcde")), true, "abcde valid");
+    assert_eq!(is_valid_2(1, 3, String::from("b"), String::from("cdefg")), false, "cdefg invalid");
+    assert_eq!(is_valid_2(2, 9, String::from("c"), String::from("caccccccc")), false, "ccccccccc valid");
+    assert_eq!(is_valid_2(2, 9, String::from("c"), String::from("cccccccca")), true, "cccccccca valid");
 }
 
 #[test]
@@ -90,4 +151,5 @@ fn test_parser() {
 #[test]
 fn test_valid_passwords() {
     assert_eq!(445, day02a());
+    assert!(250 != day02b());
 }
